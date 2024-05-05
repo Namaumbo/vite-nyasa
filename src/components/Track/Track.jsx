c4bf755fc2c84ee0383de8f47e00fee9713fcc34
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2024-05-05 21:50:37 +0200

    on hover effect

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -4,7 +4,7 @@ export default function Track() {
     <div className="track-container lg:w-96 relative">
       <img src="photo2.jpg" alt="artwork" className="w-full" id="artwork" />
       <div className="overlay absolute inset-0 flex justify-center items-center opacity-0 hover:opacity-100">
-        <img src="play.png" alt="play" className="absolute bottom-20 right-4" />
+        <img src="play.png" alt="play" className="absolute bottom-20 right-6" />
       </div>
       <div className="flex flex-col text-center ">
         <span className="text-2xl text-slate-500 font-bolder">Emtee</span>
